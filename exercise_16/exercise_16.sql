56bc451add5e0a940504b47c39901bd29100fe51
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-02-15 14:35:58 -0800

    add solution for exercise 16

--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,9 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
     
+SELECT title, description, release_year
+FROM film
+left join film_actor
+on film_actor.film_id = film.film_id
+left join actor on actor.actor_id = film_actor.actor_id
+group BY
